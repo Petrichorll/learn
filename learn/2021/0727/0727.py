31e2c02c662c0de77fe2d2a7b897213c8a7ba992
Petrichorll <690267573@qq.com>
2021-08-06 14:18:58 +0800

    0806更新

--- learn/2021/0727/0727.py
+++ learn/2021/0727/0727.py
@@ -16,33 +16,25 @@
 └─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘ └───────┴───┴───┘
 '''
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 from selenium import webdriver
 import time
 
+driver=webdriver.Edge()
 
-driver = webdriver.Edge()
+driver.get("https://cn.bing.com/images/search?view=detailV2&ccid=X1xO66w6&id=84E0DEFB8DFB823194A8F22350C5C6CC72A7126E&thid=OIP-C.X1xO66w6pYUH_N7hbTq_nQHaJP&mediaurl=https%3A%2F%2Fww1.sinaimg.cn%2Flarge%2F819cc17dly1gbu0fj5mijj20vf138tnu.jpg&exph=1412&expw=1131&q=%e6%89%a3%e7%af%ae%e5%9b%be%e7%89%87&simid=607992997865862872&form=IRPRST&ck=97668C09247D7B1EB226760587544633&selectedindex=0&ajaxhist=0&ajaxserp=0&vt=0&sim=11")
 
-driver.get("https://www.baidu.com")
+a=driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div[1]/div[2]/div[1]/div[1]/div[1]/div/div").text
+
+print(a)
 
 
-driver.quit()
+driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div[1]/div[2]/div[2]/span").click()
+
 time.sleep(1000)
 
 
+
+driver.quit()
 
 
 
